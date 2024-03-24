db079480769a176ea1f8d82e5cf97228c3e0362d
YashasHewage <yashrashh05@gmail.com>
2024-03-25 03:24:44 +0530

    theme colors

---
+++ tasktrail/android/app/src/main/kotlin/com/example/tasktrail/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tasktrail
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ tasktrail/lib/themes/dark_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
+  ),
+);
