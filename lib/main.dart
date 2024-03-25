6561672b71f6eec6119c3e8ad9ebf80429a2c8d5
YashasHewage <yashrashh05@gmail.com>
2024-03-25 19:25:40 +0530

    Auth gate

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tasktrail/auth/login_or_register.dart';
+import 'package:tasktrail/services/auth/auth_get.dart';
+import 'package:tasktrail/services/auth/login_or_register.dart';
+import 'package:tasktrail/firebase_options.dart';
 import 'package:tasktrail/themes/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+
   runApp(
+
     ChangeNotifierProvider(
       create: (context) => ThemeProvider(),
       child: const MyApp(),
@@ -18,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginOrRegister (),
+      home: const AuthGate(),
       theme: Provider.of<ThemeProvider>(context).themeData,
 
     );
---
+++ lib/services/auth/auth_get.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasktrail/pages/home_page.dart';
+import 'package:tasktrail/services/auth/login_or_register.dart';
+
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context,snapshot) {
+          if(snapshot.hasData){
+            return const HomePage();
+          }
+          else {
+            return const LoginOrRegister();
+          }
+        },
+      ),
+
+    );
+  }
+}
