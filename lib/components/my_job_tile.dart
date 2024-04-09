965a9c8383dccac7791423feb7c7b8bf2cb8c3d3
YashasHewage <yashrashh05@gmail.com>
2024-04-10 01:33:36 +0530

    card final

--- lib/components/my_job_tile.dart
+++ lib/components/my_job_tile.dart
@@ -22,137 +22,142 @@ class JobCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 365,
-      height: 133,
-      child: Stack(
-        children: [
-          Positioned(
-            left: 5,
-            top: 0,
-            child: Container(
-              width: 365,
-              height: 133,
-              decoration: ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    width: 1,
-                    color: Colors.black.withOpacity(0.2),
-                  ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                shadows: const [
-                  BoxShadow(
-                    color: Colors.white,
-                    blurRadius: 4,
-                    offset: Offset(0, 4),
-                    spreadRadius: 0,
-                  )
-                ],
-              ),
-            ),
-          ),
-          const Positioned(
-            left: 20,
-            top: 17,
-            child: SizedBox(
-              width: 203,
-              height: 31,
-              child: Text(
-                'Need a Maths Teacher',
-                style: TextStyle(
-                  color: Color(0xFF090F24),
-                  fontSize: 18,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w400,
-                  height: 0,
-                ),
-              ),
-            ),
-          ),
-          const Positioned(
-            left: 20,
-            top: 51,
-            child: SizedBox(
-              width: 150,
-              height: 14,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.location_on,
-                    size: 14,
-                  ),
-                  Text(
-                    'Gampaha, Western Province',
-                    style: TextStyle(
-                      color: Color(0xFF868686),
-                      fontSize: 10,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w400,
-                      height: 0,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Positioned(
-            left: 22,
-            top: 79,
-            child: Container(
-              width: 68,
-              height: 25,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    top: 0,
-                    child: Container(
-                      width: 68,
-                      height: 25,
-                      decoration: ShapeDecoration(
-                        color: const Color(0xE8406AFF),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(24),
-                        ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, bottom: 20),
+      child: Container(
+        width: 365,
+        height: 133,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 5,
+              top: 0,
+              child: Center(
+                child: Container(
+                  width: 365,
+                  height: 133,
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.2),
                       ),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                  ),
-                  const Center(
-                    child: Text(
-                      'Education',
-                      style: TextStyle(
+                    shadows: const [
+                      BoxShadow(
                         color: Colors.white,
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 17,
+              child: SizedBox(
+                width: 203,
+                height: 31,
+                child: Text(
+                  '$jobTitle',
+                  style: TextStyle(
+                    color: Color(0xFF090F24),
+                    fontSize: 18,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w400,
+                    height: 0,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 51,
+              child: SizedBox(
+                width: 150,
+                height: 14,
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.location_on,
+                      size: 14,
+                    ),
+                    Text(
+                      '$address',
+                      style: const TextStyle(
+                        color: Color(0xFF868686),
                         fontSize: 10,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w400,
                         height: 0,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Positioned(
-            left: 269,
-            top: 83,
-            child: SizedBox(
-              width: 71,
-              height: 27,
-              child: Text(
-                '\$10 / Hour',
-                style: TextStyle(
-                  color: Color(0xFF434F76),
-                  fontSize: 14,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600,
-                  height: 0.12,
+                  ],
                 ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              left: 22,
+              top: 79,
+              child: Container(
+                width: 68,
+                height: 25,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 0,
+                      top: 0,
+                      child: Container(
+                        width: 68,
+                        height: 25,
+                        decoration: ShapeDecoration(
+                          color: const Color(0xE8406AFF),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Text(
+                        '$jobType',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              left: 269,
+              top: 83,
+              child: SizedBox(
+                width: 71,
+                height: 27,
+                child: Text(
+                  'Rs$price',
+                  style: TextStyle(
+                    color: Color(0xFF434F76),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    height: 0.12,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import 'dart:ffi';
-
-=======
 import 'package:cloud_firestore/cloud_firestore.dart';
->>>>>>> de2d2cf77ea86cc31a088549eb8969686fc21265
 import 'package:flutter/material.dart';
 import 'package:tasktrail/components/my_drawer.dart';
 import 'package:tasktrail/components/my_job_tile.dart';
@@ -37,7 +32,7 @@ class _HomePageState extends State<HomePage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.tertiary,
+      backgroundColor: Colors.white,
       drawer: const Mydrawer(),
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled) => [
@@ -45,45 +40,14 @@ class _HomePageState extends State<HomePage>
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-<<<<<<< HEAD
-                //image insert
-=======
->>>>>>> de2d2cf77ea86cc31a088549eb8969686fc21265
                 Image.asset(
                   'assets/images/landingpage.png',
                   height: 200,
                 ),
-
-                // Divider(
-                //   indent: 25,
-                //   endIndent: 25,
-                //   color: Theme.of(context).colorScheme.secondary,
-                // ),
               ],
             ),
           ),
         ],
-<<<<<<< HEAD
-        body: Padding(
-          padding: const EdgeInsets.only(left: 24),
-          child: ListView(
-            children: const [
-              JobCard(
-                  jobTitle: "fuck",
-                  price: "fuck",
-                  availableSlots: "fuck",
-                  address: "fuck",
-                  jobType: "fuck"),
-              SizedBox(height: 20),
-              JobCard(
-                  jobTitle: "fuck",
-                  price: "afdafd",
-                  availableSlots: "fadfdads",
-                  address: "fadsfads",
-                  jobType: "dafasd"),
-            ],
-          ),
-=======
         body: StreamBuilder<QuerySnapshot>(
           stream: firestoreService.getJobs(),
           builder: (context, snapshot) {
@@ -104,10 +68,12 @@ class _HomePageState extends State<HomePage>
                   jobType: doc['category'].toString(),
                   documentId: doc.id,
                 );
+                SizedBox(
+                  height: 20,
+                );
               },
             );
           },
->>>>>>> de2d2cf77ea86cc31a088549eb8969686fc21265
         ),
       ),
     );
