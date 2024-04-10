cf51dbecec40ee4f915a69f85e9d714f4f6b35e4
Dakum Perera <dakum38@gmail.com>
2024-04-10 21:46:31 +0530

    categories view

---
+++ lib/pages/categoriesview.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+class CategoriesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.02),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(height: 10),
+                  Container(
+                    margin: EdgeInsets.only(left: 1, bottom: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Categories',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            _buildCategoryButton('Engineering', context,
+                                addIcon: true, icon: Icons.lightbulb_outline),
+                            SizedBox(height: 10),
+                            _buildCategoryButton(
+                                'Information Technology', context,
+                                addIcon: true, icon: Icons.laptop),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Healthcare', context,
+                                addIcon: true, icon: Icons.add),
+                            SizedBox(height: 10),
+                            _buildCategoryButton(
+                                'Science and Technology', context,
+                                addIcon: true, icon: Icons.science),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Engineering', context,
+                                addIcon: true, icon: Icons.lightbulb_outline),
+                            SizedBox(height: 10),
+                            _buildCategoryButton(
+                                'Information Technology', context,
+                                addIcon: true, icon: Icons.laptop),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Healthcare', context,
+                                addIcon: true, icon: Icons.add),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            _buildCategoryButton('Education', context,
+                                addIcon: true, icon: Icons.school),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Construction', context,
+                                addIcon: true, icon: Icons.construction),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Transportation', context,
+                                addIcon: true, icon: Icons.local_shipping),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Finance', context,
+                                addIcon: true, icon: Icons.attach_money),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Education', context,
+                                addIcon: true, icon: Icons.school),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Construction', context,
+                                addIcon: true, icon: Icons.construction),
+                            SizedBox(height: 10),
+                            _buildCategoryButton('Transportation', context,
+                                addIcon: true, icon: Icons.local_shipping),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryButton(String categoryName, BuildContext context,
+      {bool addIcon = false, IconData? icon}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(180, 68),
+          backgroundColor: const Color(0xFFE4E4E4),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(11),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(
+                categoryName,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            if (addIcon && icon != null) Icon(icon, color: Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
