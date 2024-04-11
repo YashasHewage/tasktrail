143a1f2c7e023a5d215d278a4102426089a3494b
Navishh <navishkanuwan11@gmail.com>
2024-04-12 02:11:52 +0530

    Updated advertisement view

--- lib/pages/job_detail.dart
+++ lib/pages/job_detail.dart
@@ -280,125 +280,125 @@ class _TaskViewState extends State<TaskView> {
                   MaterialPageRoute(builder: (context) => ViewReq()),
                 );
 
-                showModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Container(
-                      height: MediaQuery.of(context).size.height * 0.32,
-                      child: Card(
-                        child: SingleChildScrollView(
-                          child: Container(
-                            margin: const EdgeInsets.only(
-                                top: 30, left: 20, right: 20),
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  children: [
-                                    Text(
-                                      'About the Advertiser',
-                                      style: GoogleFonts.poppins(
-                                        color: const Color(0xFF928FFF),
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 20),
-                                FutureBuilder<DocumentSnapshot>(
-                                  future: firestoreService
-                                      .getUserByEmail(jobData['ownerEmail']),
-                                  builder: (context, snapshot) {
-                                    if (snapshot.connectionState ==
-                                        ConnectionState.waiting) {
-                                      return const CircularProgressIndicator();
-                                    }
-                                    if (snapshot.hasError) {
-                                      return const Text(
-                                          'Error fetching user details');
-                                    }
-                                    final userData = snapshot.data!.data()
-                                        as Map<String, dynamic>;
-                                    return Row(
-                                      children: <Widget>[
-                                        CircleAvatar(
-                                          radius: 35,
-                                          backgroundImage: AssetImage(
-                                              'assets/images/avatar${userData['imageId']}.png'),
-                                        ),
-                                        const SizedBox(width: 15),
-                                        Expanded(
-                                          child: Column(
-                                            children: <Widget>[
-                                              Row(
-                                                children: <Widget>[
-                                                  Expanded(
-                                                    child: Text(
-                                                      userData['username'],
-                                                      overflow:
-                                                          TextOverflow.visible,
-                                                      style: GoogleFonts.poppins(
-                                                        color:
-                                                            const Color.fromARGB(
-                                                                255, 27, 27, 27),
-                                                        fontSize: 16,
-                                                        fontWeight:
-                                                            FontWeight.w500,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                              const SizedBox(height: 2),
-                                              Row(
-                                                children: <Widget>[
-                                                  Expanded(
-                                                    child: Text(
-                                                      jobData['ownerEmail'],
-                                                      overflow:
-                                                          TextOverflow.visible,
-                                                      style: GoogleFonts.poppins(
-                                                        color:
-                                                            const Color.fromARGB(
-                                                                255,
-                                                                150,
-                                                                150,
-                                                                150),
-                                                        fontSize: 14,
-                                                        fontWeight:
-                                                            FontWeight.w400,
-                                                      ),
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        const CircleAvatar(
-                                          radius: 22,
-                                          backgroundColor: Color(0xFF928FFF),
-                                          child: Icon(Icons.phone, size: 22),
-                                        ),
-                                        const SizedBox(width: 10),
-                                        const CircleAvatar(
-                                          radius: 22,
-                                          backgroundColor: Color(0xFF928FFF),
-                                          child: Icon(Icons.mail, size: 22),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
+                // showModalBottomSheet(
+                //   context: context,
+                //   builder: (BuildContext context) {
+                //     return Container(
+                //       height: MediaQuery.of(context).size.height * 0.32,
+                //       child: Card(
+                //         child: SingleChildScrollView(
+                //           child: Container(
+                //             margin: const EdgeInsets.only(
+                //                 top: 30, left: 20, right: 20),
+                //             child: Column(
+                //               children: <Widget>[
+                //                 Row(
+                //                   children: [
+                //                     Text(
+                //                       'About the Advertiser',
+                //                       style: GoogleFonts.poppins(
+                //                         color: const Color(0xFF928FFF),
+                //                         fontSize: 18,
+                //                         fontWeight: FontWeight.w500,
+                //                       ),
+                //                     ),
+                //                   ],
+                //                 ),
+                //                 const SizedBox(height: 20),
+                //                 FutureBuilder<DocumentSnapshot>(
+                //                   future: firestoreService
+                //                       .getUserByEmail(jobData['ownerEmail']),
+                //                   builder: (context, snapshot) {
+                //                     if (snapshot.connectionState ==
+                //                         ConnectionState.waiting) {
+                //                       return const CircularProgressIndicator();
+                //                     }
+                //                     if (snapshot.hasError) {
+                //                       return const Text(
+                //                           'Error fetching user details');
+                //                     }
+                //                     final userData = snapshot.data!.data()
+                //                         as Map<String, dynamic>;
+                //                     return Row(
+                //                       children: <Widget>[
+                //                         CircleAvatar(
+                //                           radius: 35,
+                //                           backgroundImage: AssetImage(
+                //                               'assets/images/avatar${userData['imageId']}.png'),
+                //                         ),
+                //                         const SizedBox(width: 15),
+                //                         Expanded(
+                //                           child: Column(
+                //                             children: <Widget>[
+                //                               Row(
+                //                                 children: <Widget>[
+                //                                   Expanded(
+                //                                     child: Text(
+                //                                       userData['username'],
+                //                                       overflow:
+                //                                           TextOverflow.visible,
+                //                                       style: GoogleFonts.poppins(
+                //                                         color:
+                //                                             const Color.fromARGB(
+                //                                                 255, 27, 27, 27),
+                //                                         fontSize: 16,
+                //                                         fontWeight:
+                //                                             FontWeight.w500,
+                //                                       ),
+                //                                     ),
+                //                                   ),
+                //                                 ],
+                //                               ),
+                //                               const SizedBox(height: 2),
+                //                               Row(
+                //                                 children: <Widget>[
+                //                                   Expanded(
+                //                                     child: Text(
+                //                                       jobData['ownerEmail'],
+                //                                       overflow:
+                //                                           TextOverflow.visible,
+                //                                       style: GoogleFonts.poppins(
+                //                                         color:
+                //                                             const Color.fromARGB(
+                //                                                 255,
+                //                                                 150,
+                //                                                 150,
+                //                                                 150),
+                //                                         fontSize: 14,
+                //                                         fontWeight:
+                //                                             FontWeight.w400,
+                //                                       ),
+                //                                     ),
+                //                                   ),
+                //                                 ],
+                //                               ),
+                //                             ],
+                //                           ),
+                //                         ),
+                //                         const SizedBox(width: 10),
+                //                         const CircleAvatar(
+                //                           radius: 22,
+                //                           backgroundColor: Color(0xFF928FFF),
+                //                           child: Icon(Icons.phone, size: 22),
+                //                         ),
+                //                         const SizedBox(width: 10),
+                //                         const CircleAvatar(
+                //                           radius: 22,
+                //                           backgroundColor: Color(0xFF928FFF),
+                //                           child: Icon(Icons.mail, size: 22),
+                //                         ),
+                //                       ],
+                //                     );
+                //                   },
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //       ),
+                //     );
+                //   },
+                // );
               },
               child: Text(
                 'Enroll Now',
