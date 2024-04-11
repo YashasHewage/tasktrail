cd68fee4ee4da10162be0f12a8e0a012d57fac26
NemalFdo <dinethnemal2002@gmail.com>
2024-04-11 16:38:09 +0530

    Update Edit Profile

--- lib/components/edit_profile_model.dart
+++ lib/components/edit_profile_model.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tasktrail/pages/settings_page.dart';
+import 'package:flutter/services.dart';
 
 class EditProfile extends StatefulWidget {
   @override
@@ -11,8 +12,66 @@ class _EditProfileState extends State<EditProfile> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+      appBar: AppBar(
+        title: const Text(" Edit Profile"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            CircleAvatar(
+              radius: 70,
+              backgroundImage: AssetImage('assets/images/user.JPG'),
+            ),
+            const SizedBox(height: 20),
+            itemProfile('Name', 'nenmkm', CupertinoIcons.person),
+            const SizedBox(height: 10),
+            itemProfile('Phone', '03107085816', CupertinoIcons.phone),
+            const SizedBox(height: 10),
+            itemProfile(
+                'Address', 'abc address, xyz city', CupertinoIcons.location),
+            const SizedBox(height: 10),
+            itemProfile(
+                'Email', 'ahadhashmideveloper@gmail.com', CupertinoIcons.mail),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.all(15),
+                  ),
+                  child: const Text('Edit Profile')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  itemProfile(String title, String subtitle, IconData iconData) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 5),
+                color: Colors.deepOrange.withOpacity(.2),
+                spreadRadius: 2,
+                blurRadius: 10)
+          ]),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+        tileColor: Colors.white,
+      ),
     );
   }
 }
